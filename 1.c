3e50b3aa8519988c5bb49f9868614ca191fb25c3
Dharani1799 <46838677+Dharani1799@users.noreply.github.com>
2019-02-15 00:35:23 +0530

    1.c

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	int n,l,p,i;
+	char a[100];
+    scanf("%s",a);
+	scanf("%d",&n);
+	l=strlen(a);
+	p=l-n;
+	for(i=p;i<l;i++)
+	{
+		printf("%c",a[i]);
+	}
+}
